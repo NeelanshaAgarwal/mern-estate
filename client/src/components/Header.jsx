28d2e431b6e87db627fdc8fde59d52322d47658c
NeelanshaAgarwal <na1027@srmist.edu.in>
2024-10-26 21:13:23 +0530

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import {FaSearch} from 'react-icons/fa'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import{useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser} = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className='bg-customDarkPurple shadow-md p-2'>
         <div className='flex justify-between items-center max-w-6xl mx-auto px-3'>
@@ -11,9 +31,18 @@ export default function Header() {
         <span className='text-customGrey'>Estate</span>
         <span className='text-customPurple'>Galaxy</span>
       </h1>
-      <form className='bg-customBgSearch p-2 rounded-lg flex items-center'>
-        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 text-white placeholder-customSearchIcon'/>
-        <FaSearch className='text-customSearchIcon' />
+      <form onSubmit={handleSubmit} className='bg-customBgSearch p-2 rounded-lg flex items-center'>
+        <input 
+        type='text' 
+        placeholder='Search...' 
+        className='bg-transparent focus:outline-none w-24 sm:w-64 text-white placeholder-customSearchIcon'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button>
+          <FaSearch className='text-customSearchIcon' />
+        </button>
+        
       </form>
       <ul className='flex gap-4'>
         <Link to='/'>
